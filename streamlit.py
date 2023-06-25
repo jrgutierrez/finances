1892546310307dad4bcccf8649ae727b1b4710a6
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-25 18:35:57 +0200

    Update

--- streamlit.py
+++ streamlit.py
@@ -49,7 +49,7 @@ st.plotly_chart(fig, use_container_width=True)
 
 data = data.groupby('company').agg({'price': 'sum'})
 
-fig = px.bar(data, x = data.index, y = 'price', title = f'{data.index[0]} by company')
+fig = px.bar(data, x = data.index, y = 'price', title = f'{data.index} by company')
 fig.update_layout(xaxis_title = 'Week'if group_time == 'Week' else 'Month' if group_time == 'Month' else 'Date', 
                   yaxis_title = 'Billing')
 fig.update_traces(marker_color='green')
