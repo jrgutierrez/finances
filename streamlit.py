bb0c911b3eb8132d42c3b523b45ee0b2d17b40fd
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-25 17:19:41 +0200

    Update

--- streamlit.py
+++ streamlit.py
@@ -26,7 +26,7 @@ final_date = st.sidebar.date_input(
 
 data = data.loc[initial_date:final_date]
 
-fig = px.bar(data, x = data.index, y = 'price', markers = True, title = 'Billing evolution')
+fig = px.bar(data, x = data.index, y = 'price', title = 'Billing evolution')
 fig.update_traces(line_color='#00ff00')
 fig.update_layout(xaxis_title = 'Date', yaxis_title = 'Billing')
 st.plotly_chart(fig, use_container_width=True)
