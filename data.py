979e3bfc26dd9e76d25aa921fc12dbec27039950
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-25 12:01:08 +0200

    First commit

---
+++ data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+import gspread
+import streamlit as st
+
+
+def get_raw_data():
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    spreadsheet = gc.open_by_key(st.secrets["sheet_id"])
+    worksheet = spreadsheet.worksheet(st.secrets["sheet_name"])
+    data = pd.DataFrame(worksheet.get_all_records())
+    data['date'] = pd.to_datetime(data['date'])
+    return data.set_index('date').sort_index()
+
+def prepare_data(data):
+    data = data[['company', 'description', 'price']]
+    
+    data = data.groupby(lambda x: x.date).aggregate({'price': 'sum'})
+    return data.reindex(pd.date_range(data.index[0], data.index[-1])).interpolate()
+
+def get_data():
+    data = get_raw_data()
+    return prepare_data(data)
---
+++ streamlit.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import plotly.express as px
+from data import get_data
+from plotly.subplots import make_subplots
+
+
+st.set_page_config(layout="wide")
+
+data = get_data()
+
+
+
+
+initial_date = st.sidebar.date_input(
+    "Select initial date:",
+    data.index[0],
+    min_value=data.index[0], 
+    max_value=data.index[-1])
+
+final_date = st.sidebar.date_input(
+    "Select final date:",
+    data.index[-1],
+    min_value=initial_date, 
+    max_value=data.index[-1])
+
+
+data = data.loc[initial_date:final_date]
+
+fig = px.line(data, x = data.index, y = 'price', markers = True, title = 'Billing evolution')
+fig.update_traces(line_color='#00ff00')
+fig.update_layout(xaxis_title = 'Date', yaxis_title = 'Billing')
+st.plotly_chart(fig, use_container_width=True)
