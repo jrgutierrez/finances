694abadc8ad858ce8a9eb511cc3ccda69b4dc6fa
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-25 17:22:31 +0200

    Update

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ def prepare_data(data):
     data = data[['company', 'description', 'price']]
     
     data = data.groupby(lambda x: x.date).aggregate({'price': 'sum'})
-    return data.reindex(pd.date_range(data.index[0], data.index[-1])).fillna(50)
+    return data.reindex(pd.date_range(data.index[0], data.index[-1])).fillna(0)
 
 def get_data():
     data = get_raw_data()
