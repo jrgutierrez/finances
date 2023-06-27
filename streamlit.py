52e4f2935e42deefcb84b8d74d08c968d3f9d7df
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-27 21:16:25 +0200

    Update

--- streamlit.py
+++ streamlit.py
@@ -18,6 +18,9 @@ group_time = st.sidebar.radio(
         options=["Day", "Week", "Month"],
     )
 
+if window == 'Forecasting':
+    n_fc = st.sidebar.number_input('Insert Forecasting days:', min_value=1, max_value=60, value=15)
+
 initial_date = st.sidebar.date_input(
     "Select initial date:",
     datetime(2023, 6, 1),
@@ -92,15 +95,33 @@ def detail_plots():
     fig.update_xaxes(type='category')
     st.plotly_chart(fig, use_container_width=True)
 
-def fc_plots():
-    st.metric(label = "Total Billed", value = f"{sum(data['price']):.2f}€")
 
-    fig = px.line(data_evo, x = data_evo.index, y = 'price', markers = True, title = f'Billing evolution')
+def forecast(data, forecast_days = 15):
+    from statsmodels.tsa.arima.model import ARIMA
+    import warnings
+    warnings.filterwarnings('ignore')
+    from datetime import timedelta
+
+    y = data['price']
+
+    ARIMAmodel = ARIMA(y, order = (5, 2, 5))
+    ARIMAmodel = ARIMAmodel.fit()
+
+    y_pred = ARIMAmodel.get_forecast(forecast_days)
+    y_pred_df = y_pred.conf_int(alpha = 0.05) 
+    y_pred_df["Predictions"] = ARIMAmodel.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
+    y_pred_df.index = [data.index[-1] + timedelta(days = i) for i in range(forecast_days)]
+    return y_pred_df
+
+
+def fc_plots():
+    y_pred_out = forecast(data_evo, n_fc)
+    fig = px.line(data_evo, x = data_evo.index, y = 'price', markers = True, title = 'Billing Forecast', color_discrete_sequence=['#00ff00'])
+    fig.add_scatter(x=y_pred_out.index, y=y_pred_out['Predictions'], mode='markers+lines')
+    fig.update_traces(showlegend=False)
     fig.update_layout(xaxis_title = 'Week'if group_time == 'Week' else 'Month' if group_time == 'Month' else 'Date', 
                     yaxis_title = 'Billing')
-    fig.update_traces(line_color = '#00ff00')
     st.plotly_chart(fig, use_container_width=True)
-
 
 if window == 'Overview':
     overview_plots()
