c1d7c95e76483aa28a43a96deced51bcafbd3abf
jrgutierrez <jrgutierrezr97@gmail.com>
2023-06-27 23:21:20 +0200

    Update

--- streamlit.py
+++ streamlit.py
@@ -62,7 +62,8 @@ st.markdown(
 if window in ['Overview', 'Detail']:
     if group_time == 'Day':
         data_evo = data.groupby(lambda x: x.date).aggregate({'price': 'sum'})
-        data_evo = data_evo.reindex(pd.date_range(data_evo.index[0], data_evo.index[-1])).fillna(0)
+        data_evo = data_evo.reindex(pd.date_range(datetime(2023, 6, 1), datetime.today())).fillna(0)
+        data_evo = data_evo.loc[initial_date:final_date]
 
     if group_time == 'Week':
         data_evo = data.groupby(lambda x: x.isocalendar()[1]).aggregate({'price': 'sum'})
@@ -72,7 +73,8 @@ if window in ['Overview', 'Detail']:
 
 if window == 'Forecasting':
     data_evo = data.groupby(lambda x: x.date).aggregate({'price': 'sum'})
-    data_evo = data_evo.reindex(pd.date_range(data_evo.index.values[0], data_evo.index.values[-1])).fillna(0)
+    data_evo = data_evo.reindex(pd.date_range(datetime(2023, 6, 1), datetime.today())).fillna(0)
+    data_evo = data_evo.loc[initial_date:final_date]
 
 def overview_plots():
     st.metric(label = "Total Billed", value = f"{sum(data['price']):.2f}€")
